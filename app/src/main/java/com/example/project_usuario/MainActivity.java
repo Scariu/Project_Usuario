17d05df968d303ac1a1db9ff4569775074801f0a
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-19 22:15:25 -0400

    Creación del diseño.

--- app/src/main/java/com/example/project_usuario/MainActivity.java
+++ app/src/main/java/com/example/project_usuario/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.project_usuario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
