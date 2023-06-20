647835041be78a059ab44698ed972cd769145304
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-19 22:54:44 -0400

    Implementación de Toast al apretar botón Create Account y modificaciones al diseño.

--- app/src/main/java/com/example/project_usuario/MainActivity.java
+++ app/src/main/java/com/example/project_usuario/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.project_usuario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = findViewById(R.id.editTextName);
+        TextView lastName = findViewById(R.id.editTextLastName);
+        TextView email = findViewById(R.id.editTextEmail);
+        TextView password = findViewById(R.id.editTextTextPassword);
+
+        Button botonCrearCuenta = findViewById(R.id.buttonCreateAccount);
+
+
+        botonCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String clickName = name.getText().toString();
+                String clickLastName = lastName.getText().toString();
+                String clickEmail = email.getText().toString();
+                String clickPassword = password.getText().toString();
+
+                String texto = "Usuario: " + clickName + " " + clickLastName + " " + "Email: " + clickEmail + " " + "Contraseña: " + clickPassword;
+
+                Toast.makeText(getBaseContext(), texto, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
